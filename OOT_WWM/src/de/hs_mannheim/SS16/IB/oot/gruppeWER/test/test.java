f6c8a0b6ce1156b2ec4bfa9dab3956497a65b8d7
Patrick-DE <kampfmagier3@gmail.com>
2016-06-17 21:14:43 +0200

    commit JUNIT

---
+++ OOT_WWM/src/de/hs_mannheim/SS16/IB/oot/gruppeWER/test/test.java
@@ -0,0 +1,54 @@
+package de.hs_mannheim.SS16.IB.oot.gruppeWER.test
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Phantom
+ *
+ */
+public class test {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
