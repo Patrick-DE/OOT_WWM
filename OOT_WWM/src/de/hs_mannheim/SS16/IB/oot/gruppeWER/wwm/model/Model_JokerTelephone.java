b9e3b294bba6c1a02f8e0e8492e9cb8f77231967
IB-2015 <anusanranjan@hotmail.de>
2016-06-19 11:36:17 +0200

    TelephoneJoker mit entsprechenden Dateien

--- OOT_WWM/src/de/hs_mannheim/SS16/IB/oot/gruppeWER/wwm/model/Model_JokerTelephone.java
+++ OOT_WWM/src/de/hs_mannheim/SS16/IB/oot/gruppeWER/wwm/model/Model_JokerTelephone.java
@@ -1,32 +1,57 @@
 package de.hs_mannheim.SS16.IB.oot.gruppeWER.wwm.model;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
 public class Model_JokerTelephone extends Model_Joker {
 
     //MARK: - Methods
     public String getTelephonAnswer (Model_Question question, int[] fiftyFiftyValues) {
         boolean knowsRightAnswer = false; // determines if Joker knows right answer or not
         boolean readyToGiveAnswer = false;
-        String[][] possibleAnswers = {
-                {
-                    "Das ist doch sowas von klar. Die Antwort ist ",
-                    "Das ist einfach. Die Antwort ist ",
-                    "Wie können Sie das nicht wissen! Diesen Gefallen tue ich Ihnen sicher nicht.",
-                    "Sorry. Bin gerade beschäftigt."
-                },
-                {
-                    "Hm. Ich denke es ist ",
-                    "Ich tippe auf ",
-                    "Keinen Anschluss unter dieser Nummer.",
-                    "Keine Ahnung."
-                },
-                {
-                    "Sehr schwierige Frage. Ich tippe auf, ",
-                    "Ich bin mir nicht sicher. Aber wenn ich raten würde, würde ich diese Antwort nehmen: ",
-                    "Puh. Tut mir Leid. Keine Ahnung.",
-                    "Joa."
-                }
+        String[] dataPaths = {
+                "data/telephoneEasyGivesAnswer.dat", 
+                "data/telephoneMiddleGivesAnswer.dat",
+                "data/telephoneHardGivesAnswer.dat",
+                "data/telephoneEasyDontGivesAnswer.dat",
+                "data/telephoneMiddleDontGivesAnswer.dat",
+                "data/telephoneHardDontGivesAnswer.dat"
                 };
-        
+        String[/*Gives Answer or Not*/][/*Question difficulty*/][/*texts*/] possibleAnswers = new String[2][3][];
+        int dataPathsIndex = 0;
+        for (int i = 0; i < possibleAnswers.length; i++) {
+            for (int j = 0; j < possibleAnswers[i].length; j++) {
+                try {
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(new FileInputStream(dataPaths[dataPathsIndex]), "UTF8"));
+                    int length = 0;
+                    while(reader.ready()) {
+                        reader.readLine();
+                        length++;
+                    }
+                    reader.close();
+
+                    String[] answers = new String[length];
+                    reader = new BufferedReader(
+                            new InputStreamReader(new FileInputStream(dataPaths[dataPathsIndex++]), "UTF8"));
+                    int index = 0;
+                    while(reader.ready()) {
+                        answers[index++] = reader.readLine();
+                    }
+                    possibleAnswers[i][j] = answers;
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         String telephoneAnswer = "";
         
         if ((int) (100 * Math.random()) < 50)
@@ -41,9 +66,9 @@ public class Model_JokerTelephone extends Model_Joker {
                 knowsRightAnswer = false;
 
             if (readyToGiveAnswer)
-                telephoneAnswer = possibleAnswers[0][(int) (Math.random() * possibleAnswers[0].length / 2)];
+                telephoneAnswer = possibleAnswers[0][0][(int) (Math.random() * possibleAnswers[0][0].length)];
             else
-                telephoneAnswer = possibleAnswers[0][possibleAnswers[0].length / 2 + (int) (Math.random() * possibleAnswers[0].length / 2)];
+                telephoneAnswer = possibleAnswers[1][0][(int) (Math.random() * possibleAnswers[1][0].length)];
 
         } else if (question.getDifficultyValue() == 1) {
             if ((int) (100 * Math.random()) < 50)
@@ -52,9 +77,9 @@ public class Model_JokerTelephone extends Model_Joker {
                 knowsRightAnswer = false;
             
             if (readyToGiveAnswer)
-                telephoneAnswer = possibleAnswers[1][(int) (Math.random() * possibleAnswers[1].length / 2)];
+                telephoneAnswer = possibleAnswers[0][1][(int) (Math.random() * possibleAnswers[0][1].length)];
             else
-                telephoneAnswer = possibleAnswers[1][possibleAnswers[1].length / 2 + (int) (Math.random() * possibleAnswers[1].length / 2)];
+                telephoneAnswer = possibleAnswers[1][1][(int) (Math.random() * possibleAnswers[1][1].length)];
 
         } else if (question.getDifficultyValue() == 2) {
             if ((int) (100 * Math.random()) < 30)
@@ -63,9 +88,9 @@ public class Model_JokerTelephone extends Model_Joker {
                 knowsRightAnswer = false;
             
             if (readyToGiveAnswer)
-                telephoneAnswer = possibleAnswers[2][(int) (Math.random() * possibleAnswers[0].length / 2)];
+                telephoneAnswer = possibleAnswers[0][2][(int) (Math.random() * possibleAnswers[0][2].length)];
             else
-                telephoneAnswer = possibleAnswers[2][possibleAnswers[2].length / 2 + (int) (Math.random() * possibleAnswers[2].length / 2)];
+                telephoneAnswer = possibleAnswers[1][2][(int) (Math.random() * possibleAnswers[1][2].length)];
         }
         
         if (readyToGiveAnswer) {
